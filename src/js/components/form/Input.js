50ab112c0b0c8d4d99da29fc50359ce56bdffc99
Ivan Greqov <idgreq@bk.ru>
2022-07-04 12:22:16 +0300

    Add Input component

---
+++ src/js/components/form/Input.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Input extends Component {
+  render() {
+    const { title, type, name, placeholder } = this.props;
+
+    return (
+      <label className="block">
+        <span className="block font-medium text-slate-700">{title}</span>
+        <input
+          className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md shadow-sm text-base placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+          type={type || 'text'}
+          name={name}
+          placeholder={placeholder || title}
+          autoComplete="off"
+        />
+      </label>
+    );
+  }
+}
