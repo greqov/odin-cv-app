041821456c494e611680902b5e5ab33e36538b56
Ivan Greqov <idgreq@bk.ru>
2022-07-06 12:03:00 +0300

    Input, Textarea: rename 'title' prop to 'label'

--- src/js/components/form/Textarea.js
+++ src/js/components/form/Textarea.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 export default class Textarea extends Component {
   render() {
-    const { title, type, name, placeholder, value, handleChange } = this.props;
+    const { label, type, name, placeholder, value, handleChange } = this.props;
 
     return (
       <label className="block">
-        <span className="block font-medium text-slate-700">{title}</span>
+        <span className="block font-medium text-slate-700">{label}</span>
         <textarea
           className="mt-1 block w-full px-3 py-2 min-h-[66px] bg-white border border-slate-300 rounded-md shadow-sm text-base placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
           type={type || 'text'}
           name={name}
           value={value}
           onChange={handleChange}
-          placeholder={placeholder || title}
+          placeholder={placeholder || label}
           autoComplete="off"
         />
       </label>
